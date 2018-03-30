abc82e94895300a70d2a173cc98dc7b17d2089da
Robin Willmott <git@coffeebeandesign.com>
2018-03-30 14:32:30 +0100

    Add user variables.

--- craft/plugins/lantra/variables/LantraVariable.php
+++ craft/plugins/lantra/variables/LantraVariable.php
@@ -3,14 +3,62 @@ namespace Craft;
 
 class LantraVariable
 {
-    public function canManage()
+    public function canManage($user = null)
     {
-        $user = craft()->userSession->getUser();
+        if (is_null($user)) {
+            $user = craft()->userSession->getUser();
+        }
 
         if ($user->admin or $user->isInGroup('schemeManagers') or $user->isInGroup('companyManagers') or $user->isInGroup('teamManagers')) {
             return true;
         }
 
         return false;
+    }
+
+    public function userType($user = null)
+    {
+        if (is_null($user)) {
+            $user = craft()->userSession->getUser();
+        }
+
+        if ($user->admin) {
+            $type = 'Admin';
+        }
+        else {
+            $type = 'User';
+
+            if ($user->isInGroup('schemeManagers')) {
+                $type .= ', Scheme Manager';
+            }
+
+            if ($user->isInGroup('companyManagers')) {
+                $type .= ', Company Manager';
+            }
+
+            if ($user->isInGroup('teamManagers')) {
+                $type .= ', Team Manager';
+            }
+        }
+
+        return $type;
+    }
+
+    public function userCompany($user = null)
+    {
+        if (is_null($user)) {
+            $user = craft()->userSession->getUser();
+        }
+
+        if ( ! $user->userTeam) {
+            return null;
+        }
+
+        $criteria = craft()->elements->getCriteria(ElementType::Entry);
+        $criteria->section = 'companies';
+        $criteria->limit = 1;
+        $criteria->relatedTo = $user->userTeam;
+
+        return $criteria->first();
     }
 }
