d2cbdd52a2d262d11372fbbac5ca7314cfa30c1a
Robin Willmott <git@coffeebeandesign.com>
2018-04-26 16:00:21 +0100

    Add managerTeams and managerCompanies methods and tidy up notes.

--- craft/plugins/lantra/variables/LantraVariable.php
+++ craft/plugins/lantra/variables/LantraVariable.php
@@ -3,19 +3,23 @@ namespace Craft;
 
 class LantraVariable
 {
+    /**
+     * Check user is teamManager or companyManager
+     *
+     * @param null $user
+     * @return bool
+     */
     public function canManage($user = null)
     {
-        if (is_null($user)) {
-            $user = craft()->userSession->getUser();
-        }
-
-        if ($user->admin or $user->isInGroup('companyManagers') or $user->isInGroup('teamManagers')) {
-            return true;
-        }
-
-        return false;
+        return craft()->lantra_users->canManage($user);
     }
 
+    /**
+     * Display list of user types
+     *
+     * @param null $user
+     * @return string
+     */
     public function userType($user = null)
     {
         if (is_null($user)) {
@@ -40,6 +44,13 @@ class LantraVariable
         return $type;
     }
 
+    /**
+     * Return user company (team company)
+     *
+     * @param null $user
+     * @return BaseElementModel|null
+     * @throws Exception
+     */
     public function userCompany($user = null)
     {
         if (is_null($user)) {
@@ -56,5 +67,67 @@ class LantraVariable
         $criteria->relatedTo = $user->getContent()->userTeam;
 
         return $criteria->first();
+    }
+
+    /**
+     * @param null $userId
+     * @param bool $includeChildren
+     * @return BaseElementModel|null
+     * @throws Exception
+     */
+    public function managerCompanies($userId = null, $includeChildren = FALSE)
+    {
+        if ( ! is_null($userId)) {
+            $user = craft()->users->getUserById($userId);
+        }
+        else {
+            $user = craft()->userSession->getUser();
+        }
+
+        $companyIds = craft()->lantra_users->getCompanyManagerCompanyIds($user, $includeChildren);
+
+        if ( ! count($companyIds)) {
+            return null;
+        }
+
+        $criteria = craft()->elements->getCriteria(ElementType::Entry);
+        $criteria->section = 'companies';
+        $criteria->limit = null;
+        $criteria->id = $companyIds;
+        $criteria->fixedOrder = true;
+
+        return $criteria->find();
+    }
+
+    /**
+     * Return managaer
+     *
+     * @param null $userId
+     * @param
+     * @return BaseElementModel|null
+     * @throws Exception
+     */
+    public function managerTeams($userId = null, $includeHierarchy = FALSE)
+    {
+        if ( ! is_null($userId)) {
+            $user = craft()->users->getUserById($userId);
+        }
+        else {
+            $user = craft()->userSession->getUser();
+        }
+
+        $teamIds = craft()->lantra_users->getManagerTeamIds($user, $includeHierarchy);
+
+        if ( ! count($teamIds)) {
+            return null;
+        }
+
+        $criteria = craft()->elements->getCriteria(ElementType::Entry);
+        $criteria->section = 'teams';
+        $criteria->limit = null;
+        $criteria->id = $teamIds;
+        $criteria->fixedOrder = true;
+
+        return $criteria->find();
     }
 }
