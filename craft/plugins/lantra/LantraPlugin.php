add408a315c68bdfbc9dd7110d8dd450eb356987
Robin Willmott <git@coffeebeandesign.com>
2018-04-28 15:09:52 +0100

    Move company licence logic to service and extract logic to smaller methods.

--- craft/plugins/lantra/LantraPlugin.php
+++ craft/plugins/lantra/LantraPlugin.php
@@ -76,38 +76,12 @@ class LantraPlugin extends BasePlugin
                     craft()->request->redirect('/unit/' . $unitEntry->id);
                 }
             }
-            // handle scheme licences
+            // handle company licence changes
             if ($entry->sectionId == $this->sectionIdCompanies){
-                $oldEntry = craft()->entries->getEntryById($entry->id);
-                $existingCompanyLicences = ($oldEntry) ? $oldEntry->companyRemainingLicences : 0;
-                $globalsScheme = craft()->globals->getSetByHandle('scheme');
-                // look for change
-                if ($existingCompanyLicences != $entry->companyRemainingLicences) {
-                    $content = false;
-                    // return scheme licences
-                    if ($existingCompanyLicences > $entry->companyRemainingLicences) {
-                        $returnedLicences = $existingCompanyLicences - $entry->companyRemainingLicences;
-                        $content = ['schemeRemainingLicences' => $globalsScheme->schemeRemainingLicences + $returnedLicences];
-                    }
-                    // remove scheme licences
-                    else {
-                        $newLicences = $entry->companyRemainingLicences - $existingCompanyLicences;
-                        // check scheme remaining licences
-                        if ($newLicences > $globalsScheme->schemeRemainingLicences) {
-                            $entry->addError('companyRemainingLicences', 'There are insufficient scheme licences.');
-                            $event->performAction = false;
-                        }
-                        else {
-                            // update scheme remaining licences
-                            $content = ['schemeRemainingLicences' => $globalsScheme->schemeRemainingLicences - $newLicences];
-                        }
-                    }
-                    // update the global
-                    if ($content) {
-                        $globalsScheme->setContentFromPost($content);
-                        craft()->globals->saveContent($globalsScheme);
-                    }
-                }
+               if ( ! craft()->lantra_licence->updateCompanyLicences($entry)){
+                   $entry->addError('companyRemainingLicences', 'There are insufficient scheme licences.');
+                   $event->performAction = false;
+               }
             }
         });
 
--- craft/plugins/lantra/services/Lantra_LicenceService.php
+++ craft/plugins/lantra/services/Lantra_LicenceService.php
@@ -12,36 +12,104 @@ class Lantra_LicenceService extends BaseApplicationComponent
      */
     function assignCompanyLicence($user) {
         $companyEntry = $this->userCompany($user);
-        if ($companyEntry && $companyEntry->companyRemainingLicences) {
-            $companyEntry->setContentFromPost([
-                'companyManager' => array($companyEntry->companyManager->first()->id),
-                'companyRemainingLicences' => $companyEntry->companyRemainingLicences - 1
-            ]);
-            if ( ! craft()->entries->saveEntry($companyEntry)) {
-                return false;
-            }
-            return true;
-        }
         // return false if none remaining
-        return false;
+        if ( ! $companyEntry || ! $companyEntry->companyRemainingLicences) {
+            return false;
+        }
+        $companyEntry->setContentFromPost([
+            'companyManager' => array($companyEntry->companyManager->first()->id),
+            'companyRemainingLicences' => $companyEntry->companyRemainingLicences - 1
+        ]);
+        if ( ! craft()->entries->saveEntry($companyEntry)) {
+            return false;
+        }
+        return true;
     }
 
     /**
      * Assign a scheme licence
      *
-     * @param $user
      * @return bool
      * @throws mixed
      */
     function assignSchemeLicence() {
-        $globalsScheme = craft()->globals->getSetByHandle('scheme');
         // return false if none remaining
-        if ( ! $globalsScheme->schemeRemainingLicences) {
+        if ( ! $this->getSchemeLicences()) {
             return false;
         }
-        $content = ['schemeRemainingLicences' => $globalsScheme->schemeRemainingLicences - 1];
-        $globalsScheme->setContentFromPost($content);
-        craft()->globals->saveContent($globalsScheme);
+        $this->subtractSchemeLicences();
         return true;
+    }
+
+    /**
+     * Handle company licence changes
+     *
+     * @param $companyEntry
+     * @return bool
+     * @throws mixed
+     */
+    function updateCompanyLicences($companyEntry) {
+        // check existing company entry
+        $oldEntry = craft()->entries->getEntryById($companyEntry->id);
+        $existingCompanyLicences = ($oldEntry) ? $oldEntry->companyRemainingLicences : 0;
+        // look for change
+        if ($existingCompanyLicences != $companyEntry->companyRemainingLicences) {
+            // return scheme licences
+            if ($existingCompanyLicences > $companyEntry->companyRemainingLicences) {
+                $returnedLicences = $existingCompanyLicences - $companyEntry->companyRemainingLicences;
+                $this->addSchemeLicences($returnedLicences);
+            }
+            // remove scheme licences
+            else {
+                $schemeLicences = $this->getSchemeLicences();
+                $newLicences = $companyEntry->companyRemainingLicences - $existingCompanyLicences;
+                // not enough scheme licences
+                if ($newLicences > $schemeLicences) {
+                    return false;
+                }
+                else {
+                    $this->subtractSchemeLicences($newLicences);
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Add scheme licences
+     *
+     * @param $number
+     * @throws mixed
+     * @return mixed
+     */
+    function addSchemeLicences($number = 1) {
+        $globalsScheme = craft()->globals->getSetByHandle('scheme');
+        $content = ['schemeRemainingLicences' => $globalsScheme->schemeRemainingLicences + (int) $number];
+        $globalsScheme->setContentFromPost($content);
+        return craft()->globals->saveContent($globalsScheme);
+    }
+
+    /**
+     * Subtract scheme licences
+     *
+     * @param $number
+     * @throws mixed
+     * @return mixed
+     */
+    function subtractSchemeLicences($number = 1) {
+        $globalsScheme = craft()->globals->getSetByHandle('scheme');
+        $content = ['schemeRemainingLicences' => $globalsScheme->schemeRemainingLicences - (int) $number];
+        $globalsScheme->setContentFromPost($content);
+        return craft()->globals->saveContent($globalsScheme);
+    }
+
+    /**
+     * Get scheme licences
+     *
+     * @return int $number
+     */
+    function getSchemeLicences() {
+        $globalsScheme = craft()->globals->getSetByHandle('scheme');
+        return (int) $globalsScheme->schemeRemainingLicences;
     }
 }
