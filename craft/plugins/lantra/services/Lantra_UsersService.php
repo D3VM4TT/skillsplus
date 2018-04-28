568d48e214aa34d30fc6d25f10254e8e3605b4e7
Robin Willmott <git@coffeebeandesign.com>
2018-04-28 12:06:12 +0100

    Add scheme manager to canMange() method.

--- craft/plugins/lantra/services/Lantra_UsersService.php
+++ craft/plugins/lantra/services/Lantra_UsersService.php
@@ -15,7 +15,7 @@ class Lantra_UsersService extends BaseApplicationComponent
             $user = craft()->userSession->getUser();
         }
 
-        if ($user->admin or $user->isInGroup('companyManagers') or $user->isInGroup('teamManagers')) {
+        if ($user->admin or $user->isInGroup('schemeManagers') or $user->isInGroup('companyManagers') or $user->isInGroup('teamManagers')) {
             return true;
         }
 
