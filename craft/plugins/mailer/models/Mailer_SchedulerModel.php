9f7d956e1f50e029a916dbc7a0bb3cba06a93664
Billy <billy@billymedia.co.uk>
2018-08-07 21:27:10 +0100

    Mailer plugin, added scheduler feature.

---
+++ craft/plugins/mailer/models/Mailer_SchedulerModel.php
@@ -0,0 +1,34 @@
+<?php
+namespace Craft;
+
+class Mailer_SchedulerModel extends BaseModel
+{
+
+	/**
+	 * @access protected
+	 * @return array
+	 */
+	protected function defineAttributes()
+	{
+
+		return array(
+            'id'       		=> AttributeType::Number,
+
+            'subject'       => AttributeType::String,
+            'htmlBody'      => AttributeType::String,
+            'status'        => array(AttributeType::Enum, 'values' => "finished,running,failed"),
+            'description'   => AttributeType::String,
+
+            'dateCreated'   => AttributeType::DateTime,
+            'dateFinished'  => AttributeType::DateTime,
+
+            'success'       => AttributeType::Number,
+            'errors'        => AttributeType::Mixed,
+
+            'postData'       => AttributeType::String,
+            'dateToSend'   => AttributeType::DateTime,
+        );
+
+	}
+
+}
