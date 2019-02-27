d39dc13bf1bc98a71541077442fc0509a8805540
Robin Willmott <git@coffeebeandesign.com>
2019-02-27 14:04:21 +0000

    Add general config back into repo, no secrets.

---
+++ craft/config/general.php
@@ -0,0 +1,44 @@
+<?php
+
+$parts = explode('.', $_SERVER['HTTP_HOST']);
+$site = array_shift($parts);
+
+return array(
+    /* all environments */
+    '*' => array(
+        'environmentVariables' => array(),
+        'defaultWeekStartDay' => 0,
+        'enableCsrfProtection' => false,
+        'omitScriptNameInUrls' => true,
+        'cpTrigger' => 'admin',
+        'devMode' => false,
+        'loginPath' => '/public',
+        'setPasswordPath' => '/public/password/set',
+        'setPasswordSuccessPath' => '/',
+        'useEmailAsUsername' => false,
+        'autoLoginAfterAccountActivation' => true,
+        'environmentVariables' => array(
+            'basePath' => '/datadisk/sites/' . $site,
+        ),
+    ),
+    /* local server */
+    'cpd.lantra.local' => array(
+        'devMode' => true,
+        'siteUrl' => 'http://cpd.lantra.local',
+        'environmentVariables' => array(
+            'basePath' => '/websites/cpd.lantra.co.uk/'
+        ),
+    ),
+    /* dev server */
+    'newdev.skills-plus.net' => array(
+        'devMode' => true,
+    ),
+    /* uat server */
+    'newuat.skills-plus.net' => array(
+        'devMode' => false,
+    ),
+    /* stg server */
+    'newstg.skills-plus.net' => array(
+        'devMode' => false,
+    )
+);
