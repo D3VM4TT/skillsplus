54de3b95485a8d80a210d15c56eb720e738e6dfd
Robin Willmott <git@coffeebeandesign.com>
2019-03-20 09:59:06 +0000

    Fix basePath.

--- craft/config/general.php
+++ craft/config/general.php
@@ -18,7 +18,7 @@ return array(
         'useEmailAsUsername' => false,
         'autoLoginAfterAccountActivation' => true,
         'environmentVariables' => array(
-            'basePath' => '/datadisk/sites/' . $site . '/html/',
+            'basePath' => '/datadisk/sites/' . $site . '/',
         ),
     ),
     /* local server */
