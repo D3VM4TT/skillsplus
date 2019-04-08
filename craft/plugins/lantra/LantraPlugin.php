6cf4cbad7bb1d72d55f64b261493b754c538de76
Robin Willmott <git@coffeebeandesign.com>
2019-04-08 18:02:51 +0100

    Add dynamic settings and theme login message.

--- craft/plugins/lantra/LantraPlugin.php
+++ craft/plugins/lantra/LantraPlugin.php
@@ -181,12 +181,41 @@ class LantraPlugin extends BasePlugin
     }
 
     /**
-     * @return array
+     * @return array @note same MUST be copied to settings model
      */
     protected function defineSettings()
     {
         return array(
-            'lantraDisableLicences' => AttributeType::Bool,
+            'dateFormat'                        => AttributeType::String,
+            'defaultLimit'                      => AttributeType::Number,
+
+            ## theme settings
+            'themeLoginMessage'                 => AttributeType::String,
+
+            ## change to schemeDisableLicences?
+            'lantraDisableLicences'             => AttributeType::Bool,
+
+            ## scheme settings
+            'schemeRemainingLicences'           => AttributeType::Number,
+            'schemeExpiryDate'                  => AttributeType::DateTime,
+            'schemeTeams'                       => AttributeType::Bool,
+            'schemeUserReadOnly'                => AttributeType::Bool,
+            'schemeEmailDomain'                 => AttributeType::String,
+            'schemeTestEmailAddress'            => AttributeType::String,
+            ## individual company relationship?
+            'individualCompany'                 => AttributeType::Number,
+            'individualLicenceDays'             => AttributeType::Number,
+            'individualLicencePaypalButton'     => AttributeType::String,
+
+            ## notifications
+            'notifyFooter'                      => AttributeType::String,
+            'notifySubjectBlockedResult'        => AttributeType::String,
+            'notifySubjectEndorsementResult'    => AttributeType::String,
+            'notifySubjectLicencesRemaining'    => AttributeType::String,
+            'notifySubjectManagerSummary'       => AttributeType::String,
+            'notifySubjectModuleResult'         => AttributeType::String,
+            'notifySubjectSchemeExpiry'         => AttributeType::String,
+            'notifySubjectUserExpiry'           => AttributeType::String
         );
     }
 
