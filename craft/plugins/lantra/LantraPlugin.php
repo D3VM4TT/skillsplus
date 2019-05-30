cf7b8ffddb0aef311b8309f4d49033da1684df14
Robin Willmott <git@coffeebeandesign.com>
2019-05-30 15:56:39 +0100

    SP0193 fix default limt bug and remove update settings function.

--- craft/plugins/lantra/LantraPlugin.php
+++ craft/plugins/lantra/LantraPlugin.php
@@ -39,8 +39,6 @@ class LantraPlugin extends BasePlugin
         
         parent::init();
 
-        craft()->lantra_settings->updateSettings($this->settingsVersion);
-
         // check user licence
         craft()->on('users.onBeforeSaveUser', function(Event $event) {
             $user = $event->params['user'];
