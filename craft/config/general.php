1cccc200d83535cc337bb3281b192d479ef197ff
Robin Willmott <git@coffeebeandesign.com>
2019-05-31 17:30:47 +0100

    Make general settings default.

--- craft/config/general.php
+++ craft/config/general.php
@@ -10,7 +10,7 @@ return array(
         'enableCsrfProtection' => false,
         'omitScriptNameInUrls' => true,
         'cpTrigger' => 'admin',
-        'devMode' => true,
+        'devMode' => false,
         'loginPath' => '/public',
         'setPasswordPath' => '/public/password/set',
         'setPasswordSuccessPath' => '/',
@@ -21,7 +21,7 @@ return array(
         'environmentVariables' => array(
             'basePath' => '/datadisk/sites/' . $site . '/',
             'assetsPath' => '/datadisk/azureshare/' . $site . '/',
-            'server' => 'dev',
+            'server' => 'prod',
         ),
     ),
     /* local server */
