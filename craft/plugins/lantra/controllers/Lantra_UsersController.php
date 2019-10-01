14eddd01f60e5e0dec8720806d77b4b79ad2ecc6
Robin Willmott <git@coffeebeandesign.com>
2019-10-01 11:35:35 +0100

    SP92 add cache to hierarchy - but this time do it right.

--- craft/plugins/lantra/controllers/Lantra_UsersController.php
+++ craft/plugins/lantra/controllers/Lantra_UsersController.php
@@ -17,7 +17,13 @@ class Lantra_UsersController extends Lantra_BaseController {
         $companyId = craft()->request->getParam('companyId');
         $type = craft()->request->getParam('type');
         $user = craft()->userSession->getUser();
-        $node = craft()->lantra_structure->getHierarchy($companyId, $type, $user->id);
+        // look in cache
+        $cache = 'lantraHierarchy' . $user->id . (!$companyId ? 'root' : $companyId . $type);
+        if (false == $node = craft()->cache->get($cache)) {
+            $node = craft()->lantra_structure->getHierarchy($companyId, $type);
+            // set cache
+            craft()->cache->set($cache, $node, 86400);
+        }
         return craft()->controller->returnJson($node);
     }
 
