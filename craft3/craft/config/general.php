d20b0257d493850b6f4e43675380cfba8d9dee61
Robin Willmott <git@coffeebeandesign.com>
2019-12-10 10:55:52 +0000

    Copy Craft2 general config.

--- craft3/craft/config/general.php
+++ craft3/craft/config/general.php
@@ -8,25 +8,62 @@
  * @see \craft\config\GeneralConfig
  */
 
+$parts = explode('.', $_SERVER['HTTP_HOST']);
+$site = array_shift($parts);
+
 return [
     '*' => [
-        'defaultWeekStartDay' => 1,
+        'defaultWeekStartDay' => 0,
         'omitScriptNameInUrls' => true,
         'cpTrigger' => 'admin',
+        'devMode' => false,
+        'loginPath' => '/public',
+        'setPasswordPath' => '/public/password/set',
+        'setPasswordSuccessPath' => '/',
+        'useEmailAsUsername' => false,
         'securityKey' => getenv('SECURITY_KEY'),
         'useProjectConfigFile' => false,
-        'backupOnUpdate' => false
+        'backupOnUpdate' => false,
+        'autoLoginAfterAccountActivation' => true,
+        'phpMaxMemoryLimit' => '4096M',
+        'maxUploadFileSize' => '2147483648',
+        'environmentVariables' => array(
+            'basePath' => '/datadisk/sites/' . $site . '/',
+            'assetsPath' => '/datadisk/azureshare/' . $site . '/',
+            'server' => 'prod',
+            'site' => $site,
+        ),
     ],
     'local' => [
+        'siteUrl' => 'http://craft3.skills-plus.local',
         'devMode' => true,
+        'environmentVariables' => array(
+            'basePath' => '/websites/skills-plus.net/craft3',
+            'assetsPath' => '/websites/skills-plus.net/craft3/',
+            'server' => 'local',
+        ),
     ],
     'dev' => [
         'devMode' => true,
+        'environmentVariables' => array(
+            'server' => 'dev',
+            'site' => $site
+        ),
     ],
     'uat' => [
+        'devMode' => true,
+        'environmentVariables' => array(
+            'server' => 'uat',
+            'site' => $site
+        ),
         'allowAdminChanges' => false,
     ],
     'prod' => [
+        'devMode' => true,
+        'environmentVariables' => array(
+            'server' => 'uat',
+            'site' => $site
+        ),
         'allowAdminChanges' => false,
     ],
 ];
