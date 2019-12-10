456bdd6e58b6096cd13a96f8f4a0422ea209536b
Robin Willmott <git@coffeebeandesign.com>
2019-12-10 12:27:05 +0000

    Update login and password forms.

--- craft3/craft/config/general.php
+++ craft3/craft/config/general.php
@@ -13,6 +13,7 @@ $site = array_shift($parts);
 
 return [
     '*' => [
+        'enableCsrfProtection' => false,
         'defaultWeekStartDay' => 0,
         'omitScriptNameInUrls' => true,
         'cpTrigger' => 'admin',
