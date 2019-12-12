c3d6e1e9a162d0edce283ac4631bbb9859c63605
Robin Willmott <git@coffeebeandesign.com>
2019-12-12 16:30:15 +0000

    Fix Assets controller.

--- craft3/src/repository/sp/src/controllers/AssetsController.php
+++ craft3/src/repository/sp/src/controllers/AssetsController.php
@@ -9,8 +9,10 @@
 namespace lantra\sp\controllers;
 
 use Craft;
+use craft\errors\VolumeException;
 
 use lantra\sp\Plugin as Lantra;
+use lantra\sp\helpers\LantraHelper;
 
 class AssetsController extends BaseController
 {
@@ -27,10 +29,19 @@ class AssetsController extends BaseController
      */
     public function actionDeleteEvidence()
     {
-        $this->requireAjaxRequest();
+        $this->requireAcceptsJson();
         $fileId = Craft::$app->request->getParam('fileId');
-        $response = craft()->assets->deleteFiles([$fileId]);
-        $this->returnJson($response->getResponseData());
+        $asset = Craft::$app->assets->getAssetById($fileId);
+        $volume = $asset->getVolume();
+        $response = ['success' => true, 'message' => ''];
+        try {
+            $volume->deleteFile($asset->folderPath . $asset->filename);
+        } catch(VolumeException $exception) {
+            $response['success'] = false;
+            $response['message'] = $exception->getMessage();
+
+        }
+        $this->asJson($response);
     }
 
     /**
@@ -40,47 +51,30 @@ class AssetsController extends BaseController
      */
     public function actionUploadEvidence()
     {
-        $this->requireAjaxRequest();
-        $this->requireLogin();
-
-        if (empty($_FILES) || ! isset($_FILES['assets-upload']) || ! isset($_FILES['assets-upload']['name']) || ! isset($_FILES['assets-upload']['tmp_name'])) {
-            $data = [
+        $this->requireAcceptsJson();
+        if (empty($_FILES) || !isset($_FILES['assets-upload']) || !isset($_FILES['assets-upload']['name']) || !isset($_FILES['assets-upload']['tmp_name'])) {
+            $response = [
                 'success' => false,
                 'message' => 'Invalid upload parameters. Refresh and try again.'
             ];
-
-            return $this->returnJson($data);
+            return $this->asJson($response);
         }
-
         $fileName = $_FILES['assets-upload']['name'];
         $tmpName = $_FILES['assets-upload']['tmp_name'];
-
+        ## get folder
         $folderId = Craft::$app->request->getParam('folderId');
-        $fileLocation = AssetsHelper::getTempFilePath(pathinfo($fileName, PATHINFO_EXTENSION));
-        move_uploaded_file($tmpName, $fileLocation);
-        $response = craft()->assets->insertFileByLocalPath($fileLocation, $fileName, $folderId, AssetConflictResolution::KeepBoth);
-        IOHelper::deleteFile($fileLocation, true);
-
-        if ($response->isError()) {
-            $data = [
-                'success' => false,
-                'message' => $response->getAttribute('errorMessage')
-            ];
+        $folder = Craft::$app->assets->getFolderById($folderId);
+        ## upload file
+        $tempFolder = Craft::$app->path->tempPath;
+        $tempPath = $tempFolder . $fileName;
+        move_uploaded_file($tmpName, $tempPath);
+        ## create asset
+        $response = LantraHelper::addAsset($tempPath, $fileName, 'evidence', $folder->name);
+        ## delete the temp file
+        unlink($tempPath);
+        if ($response['asset']) {
+            $response['file'] = $response['asset']->id;
         }
-        if ($response->isConflict()) {
-            $data = [
-                'success' => false,
-                'message' => $response->getDataItem('fileName')  . ' already exists in your folder, please rename your file and try again.'
-            ];
-        }
-        if ($response->isSuccess()) {
-            $fileId = $response->getDataItem('fileId');
-            $file = craft()->assets->getFileById($fileId);
-            $data = [
-                'success' => true,
-                'file' => $file
-            ];
-        }
-        $this->returnJson($data);
+        $this->asJson($response);
     }
 }
