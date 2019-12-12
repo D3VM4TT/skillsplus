949c50bbfb403d03ac04a224ae30d2d5734cf691
Robin Willmott <git@coffeebeandesign.com>
2019-12-12 15:56:41 +0000

    Fix Migrations service.

--- craft3/src/repository/sp/src/services/Migrations.php
+++ craft3/src/repository/sp/src/services/Migrations.php
@@ -8,29 +8,47 @@
 
 namespace lantra\sp\services;
 
-use Craft;
 use craft\base\Component;
+use craft\records\Field;
+use craft\records\FieldLayout;
 
 class Migrations extends Component
 {
+
+    /**
+     * @param $groupId
+     * @param $name
+     * @param $handle
+     * @param $type
+     * @param $settings
+     * @return bool|Field
+     */
     public function addField($groupId, $name, $handle, $type, $settings)
     {
-        $field = new FieldModel();
+        $field = new Field();
         $field->groupId = $groupId;
         $field->name = $name;
         $field->handle = $handle;
         $field->translatable = true;
         $field->type = $type;
         $field->settings = $settings;
-        if (craft()->fields->saveField($field)) {
+        if ($field->save()) {
             return $field;
         }
         return false;
     }
 
+    /**
+     * @param $layoutId
+     * @param $tabId
+     * @param $fieldId
+     * @param $required
+     * @param $sortOrder
+     * @return int
+     */
     public function addLayoutField($layoutId, $tabId, $fieldId, $required, $sortOrder)
     {
-        $fieldRecord = new FieldLayoutFieldRecord();
+        $fieldRecord = new FieldLayout();
         $fieldRecord->layoutId  = $layoutId;
         $fieldRecord->tabId     = $tabId;
         $fieldRecord->fieldId   = $fieldId;
