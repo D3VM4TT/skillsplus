0bfcaef2f737b891f2ea612c246e90c60423c9f7
Robin Willmott <git@coffeebeandesign.com>
2020-01-15 16:06:02 +0000

    Move to project config.

--- craft3/src/repository/sp/src/helpers/LantraHelper.php
+++ craft3/src/repository/sp/src/helpers/LantraHelper.php
@@ -32,11 +32,12 @@ class LantraHelper
 
     /**
      * @param string $key
+     * @param string $default
      * @return mixed|null
      */
-    public static function setting($key = '')
+    public static function setting($key = '', $default = null)
     {
-        return Lantra::$app->settings->getSetting($key);
+        return Lantra::$app->settings->getSetting($key, $default);
     }
 
     /**
