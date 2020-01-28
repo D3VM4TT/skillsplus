851971f55034d117d9fa7a1ec51e1bfb08be001a
Robin Willmott <git@coffeebeandesign.com>
2020-01-28 14:56:38 +0000

    Add migration for checking content table.

---
+++ src/repository/sp/src/migrations/m200128_145211_fix_content.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace lantra\sp\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m200128_145211_fix_content migration.
+ */
+class m200128_145211_fix_content extends Migration
+{
+    /**
+     * @return bool|void
+     * @throws \yii\base\NotSupportedException
+     */
+    public function safeUp()
+    {
+        $fieldsService = Craft::$app->getFields();
+        foreach($fieldsService->getAllFields() as $field) {
+            if ($field->hasContentColumn())
+            if (!$this->db->columnExists('{{%content}}', 'field_' . $field->name)) {
+                $this->addColumn('{{%content}}', 'field_' . $field->name, $field->getContentColumnType());
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m200128_145211_fix_content cannot be reverted.\n";
+        return false;
+    }
+}
