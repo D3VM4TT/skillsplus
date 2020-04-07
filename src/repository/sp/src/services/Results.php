9061a06bec79144aadde518741cab445fb6d6279
Robin Willmott <git@coffeebeandesign.com>
2020-04-07 10:01:19 +0100

    Fix empyt result comments.

--- src/repository/sp/src/services/Results.php
+++ src/repository/sp/src/services/Results.php
@@ -393,11 +393,13 @@ class Results extends Component
     function unreadComments($result, $userId)
     {
         $unread = 0;
-        $comments = $result->resultComments->all();
-        foreach($comments as $comment) {
-            $commentAuthorId = $comment->user->one()->id;
-            if ($commentAuthorId != $userId && ! $comment->read) {
-                $unread++;
+        if ($result && $result->resultComments) {
+            $comments = $result->resultComments->all();
+            foreach ($comments as $comment) {
+                $commentAuthorId = $comment->user->one()->id;
+                if ($commentAuthorId != $userId && !$comment->read) {
+                    $unread++;
+                }
             }
         }
         return $unread;
