020ce0c421afe22a3b90edd98c79ae4879870381
Robin Willmott <git@coffeebeandesign.com>
2021-02-26 11:56:15 +0000

    LSP312 - allow comments.

--- html/assets/js/lantra.js
+++ html/assets/js/lantra.js
@@ -704,7 +704,7 @@ $(document).ready(function(){
         $('#includeHierarchyWarning').css('display', s ? 'block' : 'none');
     });
 
-    $("form.read-only :input").not('#resultComment').not('#resultCommentButton').prop("disabled", true);
+    $("form.read-only :input").not('[type="hidden"]').not('#resultComment').not('#resultCommentButton').prop("disabled", true);
     $("form.read-only .fileinput-button").hide();
     $("form.read-only .date-selector span.clear").hide();
     $("form.read-only .delete-file").click(function(e){
