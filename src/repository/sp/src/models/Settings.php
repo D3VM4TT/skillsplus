a446f4f171909346d0daa508d2a67aef7e521273
Robin Willmott <git@coffeebeandesign.com>
2021-06-21 12:23:08 +0100

    LSP345 fix roles bug.

--- src/repository/sp/src/models/Settings.php
+++ src/repository/sp/src/models/Settings.php
@@ -384,7 +384,7 @@ class Settings extends Model
         }
 
         foreach ($this->reports as $key => $report) {
-            if (is_array($report['roles'])) {
+            if (isset($report['roles']) && is_array($report['roles'])) {
                 $categories = [];
                 foreach($report['roles'] as $categoryId) {
                     if (false != $category = Craft::$app->categories->getCategoryById($categoryId)) {
