e5fcffe77a6a990d7088eb38476b46bc4827c2d8
Robin Willmott <git@coffeebeandesign.com>
2021-07-21 15:13:01 +0100

    LSP402 update taskbooks.

---
+++ src/repository/sp/src/migrations/m210721_134639_convert_taskbooks.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace lantra\sp\migrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\elements\Category;
+use craft\elements\Entry;
+
+use lantra\sp\helpers\LantraHelper;
+
+/**
+ * m210721_134639_convertTaskbooks migration.
+ */
+class m210721_134639_convert_taskbooks extends Migration
+{
+    /**
+     * @return bool|void
+     * @throws \Throwable
+     * @throws \craft\errors\ElementNotFoundException
+     * @throws \yii\base\Exception
+     */
+    public function safeUp()
+    {
+        $sectionId = LantraHelper::sectionId('taskbooks');
+        $typeId = LantraHelper::entryTypeId('taskbooks', 'taskbook');
+
+        $criteria = Category::find();
+        $criteria->group = 'moduleGroups';
+        $criteria->moduleGroupTaskbooks = true;
+        $categories = $criteria->all();
+        foreach ($categories as $category) {
+
+            $entry = new Entry();
+            $entry->sectionId = $sectionId;
+            $entry->typeId = $typeId;
+            $entry->enabled = true;
+            $entry->title = $category->title;
+
+            if (false == Craft::$app->elements->saveElement($entry)) {
+                echo "Could not create " . $category->title;
+            }
+
+            ## update content table
+            $query = $this->db->createCommand();
+            $query->update('{{%content}}', ['elementId' => $entry->id], ['elementId' => $category->id])->execute();
+
+
+            ## update packages to point to new taskbooks
+            $criteria = Entry::find();
+            $criteria->section = 'packages';
+            $criteria->relatedTo = ['targetElement' => $category->id, 'field' => 'packageModuleGroup'];
+            $packages = $criteria->all();
+            foreach ($packages as $package) {
+                $package->setFieldValue('packageTaskbook', [$entry->id]);
+                if (Craft::$app->elements->saveElement($package)) {
+                    echo "Updated package " . $package->id . "\n\n";
+                }
+            }
+
+            echo "Converted " . $category->title . "\n\n";
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m210721_134639_convertTaskbooks cannot be reverted.\n";
+        return false;
+    }
+}
