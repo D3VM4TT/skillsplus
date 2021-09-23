a6279e2eaf9f317e1c9ad6a4afd9087b55568efd
Robin Willmott <git@coffeebeandesign.com>
2021-09-23 12:08:58 +0100

    BUGFIX if result is null.

--- src/repository/sp/src/services/Results.php
+++ src/repository/sp/src/services/Results.php
@@ -532,7 +532,8 @@ class Results extends Component
                 if (null == $resultEntry = $this->getUnitResult($userId, $unitId, $recurringCycle->code)) {
                     $resultEntry = $this->createUnitResult($userId, $unitId, $moduleResultId, $recurringCycle, $startDate);
                 }
-                if (!$resultEntry->resultModuleResult || $resultEntry->resultModuleResult->one()->id != $moduleResultId) {
+                $resultModuleResult = $resultEntry->resultModuleResult->one();
+                if (!$resultModuleResult || !$resultEntry->resultModuleResult || $resultModuleResult->id != $moduleResultId) {
                     ## fix to update resultModuleResult if cycle has changed
                     $this->setResultModuleResult($resultEntry, $moduleResultId);
                 }
