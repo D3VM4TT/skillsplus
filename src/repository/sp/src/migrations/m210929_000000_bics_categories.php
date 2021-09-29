e0c73c8f8c427a8d54c26442f788e17494f63a4b
Robin Willmott <git@coffeebeandesign.com>
2021-09-29 15:23:16 +0100

    BUGFIX bics categories.

--- src/repository/sp/src/migrations/m210929_000000_bics_categories.php
+++ src/repository/sp/src/migrations/m210929_000000_bics_categories.php
@@ -5,6 +5,7 @@ namespace lantra\sp\migrations;
 use Craft;
 use craft\db\Migration;
 use craft\elements\Entry;
+use craft\elements\SuperTableBlockElement;
 
 /**
  * m210721_134639_convertTaskbooks migration.
@@ -19,23 +20,50 @@ class m210929_000000_bics_categories extends Migration
      */
     public function safeUp()
     {
+        $field = Craft::$app->fields->getFieldByHandle('packageModuleGroups');
+
         ## update packages to remove BICS
         $criteria = Entry::find();
         $criteria->section = 'packages';
         $packages = $criteria->all();
 
         foreach ($packages as $package) {
+            if ($package->packageTaskbook->one()->id == 371282)
+            echo "Converting " . $package->title . '...' ;
             foreach ($package->packageModuleGroups as $row) {
-                $ids = $row->moduleGroup->ids();
-                if (count($ids) == 2) {
-                    foreach (array_keys($ids, 307057, true) as $key) {
-                        unset($ids[$key]);
-                    }
-                    $row->setFieldValue('moduleGroup', $ids);
-                    Craft::$app->elements->saveElement($row);
-                }
+                Craft::$app->elements->deleteElement($row);
             }
+            $this->addBlock($field, $package->id, 307057, 1, $package->packageLevel);
+            $this->addBlock($field, $package->id,307104, 0, $package->packageLevel);
+            $this->addBlock($field, $package->id, 307103, 0, $package->packageLevel);
+            $this->addBlock($field, $package->id, 307105, 0, $package->packageLevel);
+            echo "[done]\n\n";
         }
+    }
+
+    /**
+     * @param $field
+     * @param $owner
+     * @param $categoryId
+     * @param int $mandatory
+     * @param int $level
+     * @throws \Throwable
+     * @throws \craft\errors\ElementNotFoundException
+     * @throws \yii\base\Exception
+     */
+    public function addBlock ($field, $owner, $categoryId, $mandatory = 0, $level = 1) {
+
+        $stepBlockType = $this->sp->getBlockTypesByFieldId($field->id)[0];
+        $block = new SuperTableBlockElement();
+        $block->fieldId = $field->id;
+        $block->ownerId = $owner->id;
+        $block->typeId = $stepBlockType->id;
+        $block->setFieldValues([
+            'moduleGroup' => [$categoryId],
+            'moduleGroupLevel' => $level,
+            'moduleGroupMandatory' => $mandatory
+        ]);
+        Craft::$app->elements->saveElement($block);
     }
 
     /**
