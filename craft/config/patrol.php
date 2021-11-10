eca23b61b9454e4464fc8c7c958e44529d8fff47
Robin Willmott <git@coffeebeandesign.com>
2021-11-10 12:29:56 +0000

    BUGFIX patrol plugin.

---
+++ craft/config/patrol.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    '*' => [
+        'primaryDomain' => null,
+        'redirectStatusCode' => 503,
+        'maintenanceModeEnabled' => false,
+        'maintenanceModePageUrl' => '/503',
+        'maintenanceModeAuthorizedIps' => [],
+        'maintenanceModeResponseStatusCode' => 503,
+    ],
+    'cbd' => [
+        'maintenanceModeEnabled' => false,
+    ],
+    'local' => [
+        'maintenanceModeEnabled' => false,
+    ],
+    'jason' => [
+        'maintenanceModeEnabled' => false,
+    ],
+    'dev' => [
+        'maintenanceModeEnabled' => false,
+    ],
+    'uat' => [
+
+    ],
+    'prod' => [
+
+    ]
+];
