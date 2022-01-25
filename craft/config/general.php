fdcad5a0c2541e7c1e4972dcbe6819d9299d8c36
Robin Willmott <git@coffeebeandesign.com>
2022-01-25 11:53:25 +0000

    BUGFIX cbd assets path.
    (cherry picked from commit 0ba0ae092dc55156a52ccc4b048c0dde555dad67)

--- craft/config/general.php
+++ craft/config/general.php
@@ -45,7 +45,7 @@ return [
         'allowAdminChanges' => true,
         'aliases' => [
             '@basePath' => '/var/www/sp.coffeebean.design',
-            '@assetsPath' => '/var/www/sp.coffeebean.design/assets/',
+            '@assetsPath' => '/var/www/sp.coffeebean.design/craft-assets/',
             '@spBaseUrl' => 'http://base.coffeebean.design/api'
         ],
     ],
