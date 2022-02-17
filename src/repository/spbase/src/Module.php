b00e55a4a3633e8d8ac2ae7b61196c9221366a9d
Robin Willmott <git@coffeebeandesign.com>
2022-02-17 17:46:52 +0000

    Add api endpoint.

--- src/repository/spbase/src/Module.php
+++ src/repository/spbase/src/Module.php
@@ -3,17 +3,27 @@ namespace lantra\spbase;
 
 use Craft;
 
+use craft\events\RegisterUrlRulesEvent;
+use craft\web\UrlManager;
 use lantra\sp\Plugin as Lantra;
 use craft\log\FileTarget;
+use yii\base\Event;
 use yii\log\Logger;
 
 class Module extends \yii\base\Module
 {
     /**
+     * @var
+     */
+    public static $module;
+
+    /**
      *
      */
     public function init()
     {
+        self::$module = $this;
+
         ## add the spbase log file
         $fileTarget = new FileTarget([
             'logVars' => [],
@@ -21,6 +31,14 @@ class Module extends \yii\base\Module
             'categories' => ['spbase']
         ]);
         Craft::getLogger()->dispatcher->targets[] = $fileTarget;
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules = array_merge($event->rules, $this->getSiteUrlRules());
+            }
+        );
 
         parent::init();
     }
@@ -50,5 +68,15 @@ class Module extends \yii\base\Module
     public static function log($message, $level = Logger::LEVEL_INFO)
     {
         Craft::getLogger()->log($message, $level, 'spbase');
+    }
+
+    /**
+     * @return array
+     */
+    private function getSiteUrlRules()
+    {
+        return [
+            'spbase/users/<action:{slug}>' => 'spbase/base/users'
+        ];
     }
 }
